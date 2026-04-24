8413642ec0ed4c926b20aeec5d375d34a5fe76c2
OUSSAMA <oussamacharef71@gmail.com>
2026-04-24 10:38:26 +0200

    Create post details page with responsive layout and consistent styling

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
 
+        // stay to load all the page to start the funcrion
+document.addEventListener('DOMContentLoaded', function () {
+        // declare variabele
     const menu_btn = document.querySelector('#menu_btn');
     const nav_links = document.querySelector('#nav_links');
     const menu_icon = document.querySelector('#menu_icon');
     const search_toggle_btn = document.querySelector('#search_toggle_btn');
     const search_box = document.querySelector('#search_box');
     const all_links = document.querySelectorAll('.nav_link');
-
+        //function open and close the menu 
     menu_btn.addEventListener('click', function () {
         nav_links.classList.toggle('open');
 
@@ -16,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
             menu_icon.classList.replace('fa-xmark', 'fa-bars');
         }
     });
+                //function open and close the search 
 
     search_toggle_btn.addEventListener('click', function () {
         search_box.classList.toggle('open');
@@ -24,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
             document.querySelector('#search_input').focus();
         }
     });
-
+        // function for add active class to navebare
     all_links.forEach(function (link) {
         link.addEventListener('click', function () {
             all_links.forEach(item => item.classList.remove('active'));
